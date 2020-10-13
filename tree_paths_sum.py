000baf9a04db98168a0fec06aa08cd1b8a67cacd
candaceyw <candaceyw@gmail.com>
2020-10-13 16:30:29 -0400

    added notes

--- tree_paths_sum.py
+++ tree_paths_sum.py
@@ -34,10 +34,23 @@
 def tree_paths_sum(root):
 
     # if root is not empty return 0
-    if root == None:
+    if root == None:  # O(1) time
         return 0
     # take the root  starting value and add it to the root left item and the root right item until
     # we've reached all the values in the tree'
-    result = root.value + tree_paths_sum(root.left) + tree_paths_sum(root.right)
+    result = root.value + tree_paths_sum(root.left) + tree_paths_sum(root.right)  # O(1) + O(1) adding for each edge
     # return the result total
     return result
+
+
+# Time, O(n)
+
+# Base case
+# This question wants to find a path from node to the leaf, so the node who satisfies must be a leaf
+# (not node.left and not nood.right)
+# I want to recursively subtract the sum by current node's value, so the leaf node of correct
+# path must have the same value of its assigned sum
+# Recursive step
+# We want to traverse all the nodes, so node.left and node.right must be called parallelly
+# One valid path is enough, so we want to use or to connect two "branches"
+# Considering each node as the root of its children, sum should be modified from its parent
